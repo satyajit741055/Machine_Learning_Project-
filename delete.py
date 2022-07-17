5d91d19a5be945cef407ae067ae8d9da4f13f080
unknown <satyajit7410@gmail.com>
2022-07-17 18:40:08 +0530

    PRoject Structure

--- delete.py
+++ delete.py
@@ -1,22 +1,6 @@
-import logging
-import os
-from datetime import datetime
+import hashlib
 
+l = ['a','b','c','d']
+l = tuple(l)
 
-LOG_DIR = 'housing_logs'
-
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-
-LOG_FILE_NAME = f'log_{CURRENT_TIME_STAMP}.log'
-print(LOG_FILE_NAME)
-
-os.makedirs(LOG_DIR,exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
-
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    filemode='w',
-    format='[%(asctime)s %(name)s - %(levelname)s - %(message)s',
-    level = logging.INFO
-)
+print(hash(l))
