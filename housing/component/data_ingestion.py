28782fc67a172fb3622ba5ff94b6449a631f8772
unknown <satyajit7410@gmail.com>
2022-07-19 12:32:24 +0530

    Main configuration started

---
+++ housing/component/data_ingestion.py
@@ -0,0 +1,19 @@
+from housing.entity.config_entity import DataIngestionConfig
+from housing.exception import HousingException
+from housing.logger import logging
+import sys,os
+
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config = data_ingestion_config
+
+        except Exception as e:
+            raise HousingException(e,sys)
+
+    de
+        
+    
---
+++ housing/config/configuration.py
@@ -0,0 +1,44 @@
+from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig,   \
+ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
+from housing.util.util import read_yaml_file
+from housing.constant import *
+from housing.exception import HousingException
+
+
+
+
+
+class Configuartion:
+    def __init__(self,
+        config_file_path:str =CONFIG_FILE_PATH,
+        current_time_stamp:str = CURRENT_TIME_STAMP
+        ) -> None:
+        try:
+            self.config_info  = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+
+       
+    def get_data_ingestion_config(self) ->DataIngestionConfig:
+        pass
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        pass
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        pass
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        pass
+
+    def get_model_evaluation_config(self) ->ModelEvaluationConfig:
+        pass
+
+    def get_model_pusher_config(self) -> ModelPusherConfig:
+        pass
+    
+    def get_training_pipeline_config(self) ->TrainingPipelineConfig:
+        pass
